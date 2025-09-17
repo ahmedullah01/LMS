5c9abb47f8c8a0e0319f0c7b63930bc0b48b3278
ahmedullah01 <ahmedullah2095@gmail.com>
2025-09-17 22:33:53 +0500

    Initial Commit

--- src/Main.java
+++ src/Main.java
@@ -4,17 +4,17 @@ public class Main {
     public static void main(String[] args) {
         Courses c1 = new Courses();
         c1.Sem1();
-        Student phela = new Student("ahmed", "B24110006081", c1);
-        Student third = new Student("sohail", "B24110006041", c1);
-        Student fourth = new Student("rafay", "B24110006090", c1);
-        Student dosra = new Student("ali ahmed", "B24110006061", c1);
+        Student cs1 = new Student("ahmed", "B24110006081", c1);
+        Student cs2 = new Student("sohail", "B24110006041", c1);
+        Student cs3 = new Student("rafay", "B24110006090", c1);
+        Student cs4 = new Student("ali ahmed", "B24110006061", c1);
 Student_List semester_1 = new Student_List();
-semester_1.AddStudent(phela);
-semester_1.AddStudent(third);
-semester_1.AddStudent(fourth);
-        semester_1.AddStudent(dosra);
+semester_1.AddStudent(cs1);
+semester_1.AddStudent(cs2);
+semester_1.AddStudent(cs3);
+        semester_1.AddStudent(cs4);
         Collections.sort(semester_1.StudentList);
-        semester_1.RemoveStudent(phela);
+        semester_1.RemoveStudent(cs1);
         for(Student value : semester_1){
             System.out.println(value);
         }
